280739031d1e04842a4fb367c2decd76ca33f917
SirMathays <mattisuoraniemi@gmail.com>
2018-04-23 22:36:45 +0300

    Learn to read & env app_version usage

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function() {
 		Route::get('/edit/{actId}', 'ActivityController@edit')->name('editActivity');
 		Route::get('/{actId}', 'ActivityController@show')->name('showActivity');
 		Route::post('/save', 'ActivityController@save');
-		Route::post('/delete', 'ActivityController@delete');
+		Route::post('/delete/{rowId}', 'ActivityController@delete');
 	});
 
 	Route::get('/history/{year}/{month?}', 'ActivityController@history')->name('history');
@@ -38,7 +38,7 @@ Route::middleware('auth')->group(function() {
 	Route::prefix('user')->group(function() {
 		Route::get('/settings', 'UserController@edit')->name('userSettings');
 		Route::get('/{rowId}', 'UserController@profilePage')->name('profile');
-		Route::post('/save/{rowId}', 'UserController@save');
+		Route::post('/save', 'UserController@save');
 	});
 
 	// , 'namespace' => 'Api\V1', 'as' => 'api.'
